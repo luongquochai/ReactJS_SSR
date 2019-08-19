779bf8d3206ffd8921a910374a748176d51ec7e8
luongquochai <51089030+luongquochai@users.noreply.github.com>
2019-08-20 03:22:00 +0700

    Update

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+var React = require ('react');
+var ReactDOM = require ('react-dom');
+
+ReactDOM.render(
+  <a href="https://facebook.com/lqhaiii">Fb:LuongQuocHai</a>,
+  document.getElementById('root')
+)
---
+++ app/comps/hello.js
@@ -0,0 +1,4 @@
+function sayHello(){
+  document.write('Hello, I am ReactJS');
+}
+module.exports = sayHello;
